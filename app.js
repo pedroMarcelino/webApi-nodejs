13d648fbf9b342c76264b81f9914ecd6f12f8f20
pedroMarcelino <pedor.marcelinodv@gmail.com>
2024-07-28 20:56:56 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import tabela2024 from './tabela.js';
+
+const app = express();
+
+app.listen(300, () => console.log('servidor rodando com sucesso'));
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send(tabela2024)
+});
+
+app.get('/:siglaReq',(requisicao, resposta)=>{
+    const siglaReq = requisicao.params.siglaReq.toUpperCase();
+    const time = tabela2024.find(infoTime => infoTime.sigla === siglaReq)
+    resposta.send(time)
+
+    
+});
---
+++ tabela.js
@@ -0,0 +1,20 @@
+const tabela2024 = [
+    {
+        nome: 'Santos',
+        sigla: 'SAN',
+        vitorias: 0,
+        perdas: 0,
+        golsMarcados: 0,
+        golsSofridos: 0,
+    },
+    {
+        nome: 'Fluminense',
+        sigla: 'FLU',
+        vitorias: 0,
+        perdas: 0,
+        golsMarcados: 0,
+        golsSofridos: 0,
+    }
+];
+
+export default tabela2024;
